c5d38d70eb8defd3c9fc9d7cd498c92683fa4e1e
Christopher Mark Gore <cgore@cgore.com>
2013-11-05 17:40:54 -0600

    Adding a slide and example for keypresses.

---
+++ source/quit-button.lisp
@@ -0,0 +1,78 @@
+;;;; Copyright (C) 2013, Christopher Mark Gore,
+;;;; Soli Deo Gloria,
+;;;; All rights reserved.
+;;;;
+;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
+;;;; Web: http://cgore.com
+;;;; Email: cgore@cgore.com
+;;;;
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions are met:
+;;;;
+;;;;     * Redistributions of source code must retain the above copyright
+;;;;       notice, this list of conditions and the following disclaimer.
+;;;;
+;;;;     * Redistributions in binary form must reproduce the above copyright
+;;;;       notice, this list of conditions and the following disclaimer in the
+;;;;       documentation and/or other materials provided with the distribution.
+;;;;
+;;;;     * Neither the name of Christopher Mark Gore nor the names of other
+;;;;       contributors may be used to endorse or promote products derived from
+;;;;       this software without specific prior written permission.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
+
+
+(require :asdf)
+(require :sigma)
+(require :cffi)
+(load "~/quicklisp/setup.lisp")
+(require :cl-opengl)
+(require :cl-glu)
+(require :cl-glut)
+
+(defclass quit-button-window (glut:window)
+  ()
+  (:default-initargs :width 500 :height 500 :title "quit-button.lisp"
+                     :mode '(:single :rgb)))
+
+(defmethod glut:display-window :before ((w quit-button-window))
+  (gl:clear-color 0 0 0 0)
+  (gl:shade-model :flat))
+
+(defmethod glut:display ((w quit-button-window))
+  (gl:clear :color-buffer)
+  (gl:color 1 1 1)
+  (gl:load-identity) ; clear the matrix
+  ;; viewing transformation
+  (glu:look-at 0 0 5 0 0 0 0 1 0)
+  ;; modeling transformation
+  (gl:scale 1 1 1)
+  (glut:wire-cube 1)
+  (gl:flush))
+
+(defmethod glut:reshape ((w quit-button-window) width height)
+  (gl:viewport 0 0 width height)
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (gl:frustum -1 1 -1 1 1.5 20)
+  (gl:matrix-mode :modelview))
+
+
+(defmethod glut:keyboard ((w quit-button-window) key x y)
+  (declare (ignore x y))
+  (when (eql key #\Esc)
+    (glut:destroy-current-window)))
+
+(defun quit-button ()
+  (glut:display-window (make-instance 'quit-button-window)))
